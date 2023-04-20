bf6b5a3a6a6c0939e3ef19a4a1beba86127886d0
Aditya singh <107112101+its-me-aditya@users.noreply.github.com>
2023-04-20 23:28:39 +0530

    max_of_four.cpp

---
+++ max_of_four.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+int max_of_four(int a, int b, int c, int d){
+    int ans1= max(a,b);
+    int ans2=max(c,d);
+    int ans3=max(ans1,ans2);
+    return ans3;
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
