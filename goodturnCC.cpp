4fa22fa2cb399f25675ccbb35d257b6baacc1c48
Aditya singh <107112101+its-me-aditya@users.noreply.github.com>
2023-04-22 22:24:24 +0530

    goodturnCC.cpp

---
+++ goodturnCC.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++){
+    int x;
+    cin>>x;
+    int y;
+    cin>>y;
+    if (x+y>6){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+        
+    }
+    }   
+	// your code goes here
+	return 0;
+}
