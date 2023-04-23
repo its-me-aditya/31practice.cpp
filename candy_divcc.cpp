69a53d7c23e52a1be688a6deef1f3dab29f4f612
Aditya singh <107112101+its-me-aditya@users.noreply.github.com>
2023-04-23 20:22:18 +0530

    candy_divcc.cpp

---
+++ candy_divcc.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int c;
+        cin>>c;
+        if(c%3==0){
+            cout<<"YES"<<endl;
+            
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+	// your code goes here
+	return 0;
+}
