1012c7823cb5b3142af977150260aba92e3c6d71
Aditya singh <107112101+its-me-aditya@users.noreply.github.com>
2023-04-26 22:41:45 +0530

    justonemoreE.cpp

---
+++ justonemoreE.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        if(x<=24){
+            cout<<"No"<<endl;
+        }
+        else  {
+            cout<<"Yes"<<endl;
+        }
+        
+        
+    }
+	// your code goes here
+	return 0;
+}
