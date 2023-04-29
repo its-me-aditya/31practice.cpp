35746c094bb963ee5d9ebdc0eacebad39ce9b581
Aditya singh <107112101+its-me-aditya@users.noreply.github.com>
2023-04-29 08:05:47 +0530

    readpages.cpp

---
+++ readpages.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int x;
+        cin>>x;
+        int y;
+        cin>>y;
+        if(n<=x*y){
+            cout<<"YES"<<endl;
+            
+            
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+	// your code goes here
+	return 0;
+}
